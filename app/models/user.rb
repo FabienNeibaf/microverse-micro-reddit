098b4301a8f69dc43476c6b19bd4e506b94e9291
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-05-15 12:09:22 +0100

    Playing with Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  
   has_many :posts
   has_many :comments
+
+  validates :login, presence: true,
+                    length: { minimum: 6 },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+  validates :name, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should be not valid because is empty" do
+    u = User.new
+    assert_not u.valid?
+  end
+
+  test "login should have at least 4 character" do
+    u = User.new(login: 'bar', password: 'bbb', name: 'Foo')
+    assert_not u.valid?
+  end
+
+  test "login should be unique" do
+    u = User.new(login: 'barfoo', password: 'bbb', name: 'Foo')
+    dup_u = u.dup
+    u.save
+    assert_not dup_u.valid?
+  end
+  
 end
