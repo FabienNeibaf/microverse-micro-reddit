c1691a85cf0f0ae427bf29963c31b7e4cb795ae2
FabienNeibaf <fabienlintone@gmail.com>
2019-05-16 12:52:27 +0300

    Add posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :link, presence: true, length: { minimum: 10 }
+  validates :user, presence: true
+  validates_associated :comments
 end
